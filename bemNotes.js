c249686e27506abe10d60ba907016b21f27d0ceb
KITT <58197108+donaldkaiserjr@users.noreply.github.com>
2021-12-12 20:10:33 -0800

    Update bemNotes.js

--- bemNotes.js
+++ bemNotes.js
@@ -12,10 +12,14 @@
 
 // **Since Modifiers are verbose, many opt to use is-* flags instead as modifiers.
 <nav class="navbar">
-  <a href="/" class=".navbar__link.is-active"></a>
+  <a href="/" class=".navbar__link is-active"></a>
   <a href="/" class="navbar__link"></a>
   <a href="/" class="navbar__link"></a>
 </nav>
 
 .navbar__link.is-active {
 }
+
+
+// remember is-active must be chained to the Element and never alone in CSS, or there will be scope issues. Example:
+// CSS.....       .navbar__link.is-active {}
